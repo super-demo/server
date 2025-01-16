03f81919a545e0f4e3184bc548cb9235b336f14f
Nopnapat Norasri <nopnapatn@gmail.com>
2025-01-16 21:40:50 +0700

    test(*): make test

---
+++ internal/core/handlers/test/organization_category_user_test.go
@@ -0,0 +1,7 @@
+package handlers_test
+
+import "testing"
+
+func TestCreateOrganizationCategoryUser(t *testing.T) {
+	t.Skip("skipping test")
+}
---
+++ internal/core/usecases/test/organization_category_user_test.go
@@ -0,0 +1,7 @@
+package usecases_test
+
+import "testing"
+
+func TestCreateOrganizationCategoryUser(t *testing.T) {
+	t.Skip("skipping test")
+}
