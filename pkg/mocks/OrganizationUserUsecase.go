addb76677d8a6886579cb1df63a3574099a75019
Nopnapat Norasri <nopnapatn@gmail.com>
2025-01-19 00:30:16 +0700

    test(*): make test

--- pkg/mocks/OrganizationUserUsecase.go
+++ pkg/mocks/OrganizationUserUsecase.go
@@ -43,6 +43,24 @@ func (_m *OrganizationUserUsecase) CreateOrganizationUser(organizationUser *mode
 	return r0, r1
 }
 
+// DeleteOrganizationUser provides a mock function with given fields: organizationUser, requesterUserId
+func (_m *OrganizationUserUsecase) DeleteOrganizationUser(organizationUser *models.OrganizationUser, requesterUserId int) error {
+	ret := _m.Called(organizationUser, requesterUserId)
+
+	if len(ret) == 0 {
+		panic("no return value specified for DeleteOrganizationUser")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(*models.OrganizationUser, int) error); ok {
+		r0 = rf(organizationUser, requesterUserId)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // NewOrganizationUserUsecase creates a new instance of OrganizationUserUsecase. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
 // The first argument is typically a *testing.T value.
 func NewOrganizationUserUsecase(t interface {
