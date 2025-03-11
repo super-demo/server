033eb3a3849100e943b2d8e8f704c32f984a267f
Nopnapat Norasri <nopnapatn@gmail.com>
2025-03-11 23:08:29 +0700

    fix(models): fix models

--- internal/core/models/site_mini_app.go
+++ internal/core/models/site_mini_app.go
@@ -1,18 +1,22 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type SiteMiniApp struct {
-	SiteMiniAppId int       `json:"site_mini_app_id" gorm:"column:site_mini_app_id; primaryKey"`
-	SiteId        int       `json:"site_id" gorm:"column:site_id"`
-	Slug          string    `json:"slug" gorm:"column:slug"`
-	Description   string    `json:"description" gorm:"column:description"`
-	LinkUrl       string    `json:"link_url" gorm:"column:link_url"`
-	ImageUrl      string    `json:"image_url" gorm:"column:image_url"`
-	IsActive      bool      `json:"is_active" gorm:"column:is_active"`
-	CreatedAt     time.Time `json:"created_at" gorm:"column:created_at"`
-	CreatedBy     int       `json:"created_by" gorm:"column:created_by"`
-	UpdatedAt     time.Time `json:"updated_at" gorm:"column:updated_at"`
-	UpdatedBy     int       `json:"updated_by" gorm:"column:updated_by"`
-	DeletedAt     time.Time `json:"deleted_at" gorm:"column:deleted_at"`
+	SiteMiniAppId int            `json:"site_mini_app_id" gorm:"column:site_mini_app_id; primaryKey"`
+	SiteId        int            `json:"site_id" gorm:"column:site_id"`
+	Slug          string         `json:"slug" gorm:"column:slug"`
+	Description   string         `json:"description" gorm:"column:description"`
+	LinkUrl       string         `json:"link_url" gorm:"column:link_url"`
+	ImageUrl      string         `json:"image_url" gorm:"column:image_url"`
+	IsActive      bool           `json:"is_active" gorm:"column:is_active"`
+	CreatedAt     time.Time      `json:"created_at" gorm:"column:created_at"`
+	CreatedBy     int            `json:"created_by" gorm:"column:created_by"`
+	UpdatedAt     time.Time      `json:"updated_at" gorm:"column:updated_at"`
+	UpdatedBy     int            `json:"updated_by" gorm:"column:updated_by"`
+	DeletedAt     gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at"`
 }
