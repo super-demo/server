6d0aca1cfd10c67090707ab4718330e14fa175be
Nopnapat Norasri <nopnapatn@gmail.com>
2025-03-23 16:53:56 +0700

    fix(*): make new mock seeder

--- internal/adapters/databases/migrations/20250210090315_create_site_mini_apps_table.sql
+++ internal/adapters/databases/migrations/20250210090315_create_site_mini_apps_table.sql
@@ -19,8 +19,8 @@ CREATE TABLE site_mini_apps (
     FOREIGN KEY (updated_by) REFERENCES users(user_id)
 );
 
-INSERT INTO site_mini_apps (site_id, slug, description, link_url, image_url, created_by, updated_by) VALUES (1, 'Ku Book', 'Mini app 1 for test usecases', 'http://localhost:3002', 'https://scontent-bkk1-2.xx.fbcdn.net/v/t39.30808-6/476509205_555409170843779_2189911961991409505_n.jpg?stp=dst-jpg_s1080x2048_tt6&_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=dvVyY7fKDE4Q7kNvgEUsxY5&_nc_oc=Adm7pMZkVfAy3GQbposPMK9VpKk81CyRhNdrr2JqPgAp0TnQlYyuOJT7nelQ2oDVmBM&_nc_zt=23&_nc_ht=scontent-bkk1-2.xx&_nc_gid=eBOCDkseztFEgrAKMda5Xg&oh=00_AYGH3QWile1EBMI5YsbJZHdZDO8Ut6TAyNEjyCzqITjDEw&oe=67DF7CDD', 1, 1);
-INSERT INTO site_mini_apps (site_id, slug, description, link_url, image_url, created_by, updated_by) VALUES (1, 'Ku Research', 'Mini app 2 for test usecases', 'http://localhost:3003', 'https://scontent-bkk1-2.xx.fbcdn.net/v/t39.30808-6/476509205_555409170843779_2189911961991409505_n.jpg?stp=dst-jpg_s1080x2048_tt6&_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=dvVyY7fKDE4Q7kNvgEUsxY5&_nc_oc=Adm7pMZkVfAy3GQbposPMK9VpKk81CyRhNdrr2JqPgAp0TnQlYyuOJT7nelQ2oDVmBM&_nc_zt=23&_nc_ht=scontent-bkk1-2.xx&_nc_gid=eBOCDkseztFEgrAKMda5Xg&oh=00_AYGH3QWile1EBMI5YsbJZHdZDO8Ut6TAyNEjyCzqITjDEw&oe=67DF7CDD', 1, 1);
+INSERT INTO site_mini_apps (site_id, slug, description, link_url, image_url, created_by, updated_by) VALUES (1, 'Ku Book', 'Mini app 1 for test usecases', 'http://localhost:3002', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/svgs/solid/book.svg', 1, 1);
+INSERT INTO site_mini_apps (site_id, slug, description, link_url, image_url, created_by, updated_by) VALUES (1, 'Ku Research', 'Mini app 2 for test usecases', 'http://localhost:3003', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/svgs/regular/file.svg', 1, 1);
 -- +goose StatementEnd
 
 -- +goose Down
